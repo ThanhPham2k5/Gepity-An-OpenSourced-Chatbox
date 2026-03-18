07cbdc018f55eacadc1606c82c4d2335fbc4aef5
Pham Nguyen Phu Thanh <thanhphamnguyenphu2k5@gmail.com>
2026-03-18 03:19:55 +0000

    Init(Project): complete initialization

--- Gepity/app.py
+++ Gepity/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="SmartDoc AI", layout="wide")
 
-st.title("🤖 SmartDoc AI")
+st.title("🤖 Gepity")
 st.caption("Upload PDF và đặt câu hỏi")
 
 # Sidebar
