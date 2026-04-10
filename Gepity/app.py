834d0fd52202386d200b5c4455943f4c580bafb1
JustAnother-12 <bathiennguyen024@gmail.com>
2026-04-10 10:40:54 +0700

    chore: change base URL to localhost

--- Gepity/app.py
+++ Gepity/app.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title="Gepity AI", layout="wide")
 llm = OllamaLLM(
     # model="qwen2.5:7b",
     model="qwen2.5:3b",
-    base_url="http://172.25.64.1:11434"
+    base_url="http://localhost:11434"
 )
 
 # insert css
